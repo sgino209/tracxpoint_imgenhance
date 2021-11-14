fa629b5185e845a4b09add37e74efa41c533957d
Shahar Gino <danitgino@yahoo.com>
2021-11-14 14:42:43 +0200

    pipeline enhancements

--- image_enhancement.py
+++ image_enhancement.py
@@ -5,8 +5,15 @@ import tensorflow as tf
 from pathlib import Path
 from os import path, makedirs
 import matplotlib.pyplot as plt
+from warnings import simplefilter
 from PIL import Image, ImageEnhance
+import imquality.brisque as brisque
 from tensorflow.python.ops.numpy_ops import np_config
+print('OpenCV version: %s' % cv2.__version__)
+print('Tensorflow version: %s' % tf.__version__)
+
+# ignore all future warnings
+simplefilter(action='ignore', category=FutureWarning)
 
 data_dir = '/Users/shahargino/data/tracxpoint__data'
 img_files_list = [str(x) for x in Path(data_dir).rglob('*.tif')]
@@ -56,7 +63,7 @@ def clahe(img, grid_size=(8,8)):
 
 # -----------------------------------------------------------------------
 
-def gamma_correction(img, gamma=2.2):
+def gamma_correction(img, gamma=0.001):
   """ Apply Gamma Correction, so that Out = MaxVal*(In/MaxVal)**(1/gamma)"""
 
   max_val = np.iinfo(img.dtype).max
@@ -98,7 +105,7 @@ def colorize(image, hue):
 
 # -----------------------------------------------------------------------
 
-def saturation(img, saturation_factor=3):
+def saturation(img, saturation_factor=1.1):
   """ Image Saturation, based on PIL library """
 
   pil_img = Image.fromarray(img)
@@ -121,24 +128,58 @@ def sharpening(img):
 
 # -----------------------------------------------------------------------
 
-def denoise(img, h=10, template_win=7, search_win=21):
-  """ Non-local Means Denoising algorithm to remove noise in the image """
+def denoise(img, mode='bilateral', median_kernel=11, d=9, sigmaColor=75, sigmaSpace=75):
+  """ Applies the bilateral filter to an image, highly effective in noise removal while keeping edges sharp """
 
-  res = cv2.fastNlMeansDenoisingColored(img, None, h, h, template_win, search_win)
+  if mode == 'median':
+    res = cv2.medianBlur(img, median_kernel)
+  
+  elif mode == 'bilateral':
+    res = cv2.bilateralFilter(img, d, sigmaColor, sigmaSpace)
+
+  else:
+    res = img.copy()
 
   return res
 
 # -----------------------------------------------------------------------
 
+def nl_denoise(img, h=10, template_win=7, search_win=21, temporal_index=2, temporal_window=3):
+  """ Non-local Means Denoising algorithm to remove noise in the image
+      If img is a list of images, then temporal information will be exploit.
+      For example, if img is a list of 5 frames, and temporal_index=2 and 
+      temopral_window=3 then frame-1, frame-2 and frame-3 are used to denoise frame-2 """
+
+  if type(img) == list:
+    res = cv2.fastNlMeansDenoisingColoredMulti(img, temporal_index, temporal_window, None, h, h, template_win, search_win)
+
+  else:
+    res = cv2.fastNlMeansDenoisingColored(img, None, h, h, template_win, search_win)
+
+  return res
+
+# -----------------------------------------------------------------------
+
+def iqa_score(img, resize=(380, 507)):
+  """ Image Quality Assessment with no-reference, return the BRISQUE score """
+  
+  resized_img = cv2.resize(img, resize)
+  iqa_score = brisque.score(resized_img)
+
+  return iqa_score
+
+# -----------------------------------------------------------------------
+
 def image_enhance(img, gamma=0.001, sat=1.1):
-  """ Gamma Correction + Histogram Equalization + CLAHE + Sharpening + Denoise + Saturation """
+  """ Gamma Correction + Histogram Equalization + CLAHE + Local Denoise + Sharpening + NonLocal Denoise + Saturation """
 
   gamma_img = gamma_correction(img, gamma)
   histeq_img = histeq(gamma_img.astype(np.uint8))
   clahe_img = clahe(histeq_img)
-  sharp_img = sharpening(clahe_img)
-  denoise_img = denoise(sharp_img)
-  sat_img = saturation(denoise_img, sat)
+  denoise_img = denoise(clahe_img)  
+  nl_denoise_img = nl_denoise(denoise_img)
+  sharp_img = sharpening(nl_denoise_img)
+  sat_img = saturation(sharp_img, sat)
 
   return sat_img
 
@@ -147,18 +188,21 @@ def image_enhance(img, gamma=0.001, sat=1.1):
 print('Started')
 
 for k, img_file in enumerate(img_files_list):
-  
+
   print('Processing (%d/%d): %s' % (k+1, len(img_files_list), img_file))
 
-  tif_img = cv2.imread(img_file, cv2.IMREAD_UNCHANGED)
-  img = cv2.cvtColor(tif_img, cv2.COLOR_BAYER_BG2BGR) 
+  bayer_img = cv2.imread(img_file, cv2.IMREAD_UNCHANGED)
+  tif_img = cv2.cvtColor(bayer_img, cv2.COLOR_BAYER_BG2BGR)
 
-  res_img = image_enhance(img)
+  res_img = image_enhance(tif_img)
 
-  out_file = img_file.replace(data_dir, 'results')
+  tif_score = iqa_score(tif_img.astype(np.uint8))
+  res_score = iqa_score(res_img)
+
+  out_file = img_file.replace(data_dir, 'results').replace('.tif', '_iqa_%.2f_to_%.2f.tif' % (tif_score, res_score))
   out_dir = path.dirname(out_file)
   if not path.exists(out_dir):
-    makedirs(out_dir)  
+    makedirs(out_dir)
   cv2.imwrite(out_file, res_img)
 
 # -----------------------------------------------------------------------
